d5ed2aebd7603e53286d568301961b66bb0b7684
TaaviM88Laptop <juomakuski@hotmail.com>
2018-03-27 12:05:32 +0300

    Gamemanager pohja poolmanager pohja

---
+++ Contra-Clone/Assets/Scripts/GameTypes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GameTypes{
+	 public enum PlayerType
+ 	{
+	 none, player1, player2
+	}
+ }
+
---
+++ Contra-Clone/Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum PlayerType
+{
+	player1, player2
+}
+
+public class Gamemanager : MonoBehaviour {
+public static Gamemanager gamemanager;
+public GameTypes.PlayerType _player;
+	// Use this for initialization
+	void Awake () {
+		gamemanager = this;
+		//pitää vielä testa toimiiko oikeasti
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
